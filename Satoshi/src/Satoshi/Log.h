49063fe6c18aeb759e7a0baa7b7f023e76908193
Bruno Silva Marques <bmarques@studentsforliberty.org>
2021-04-16 02:08:10 -0300

    Added flush to log files that allows read it before the end of the app execution

--- Satoshi/src/Satoshi/Log.h
+++ Satoshi/src/Satoshi/Log.h
@@ -27,18 +27,18 @@ namespace Satoshi
 
 }
 
-#define ST_CORE_TRACE(...) ::Satoshi::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define ST_CORE_DEBUG(...) ::Satoshi::Log::GetCoreLogger()->debug(__VA_ARGS__)
-#define ST_CORE_INFO(...) ::Satoshi::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define ST_CORE_WARN(...) ::Satoshi::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define ST_CORE_ERR(...) ::Satoshi::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define ST_CORE_CRITICAL(...) ::Satoshi::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define ST_CORE_TRACE(...) ::Satoshi::Log::GetCoreLogger()->trace(__VA_ARGS__); ::Satoshi::Log::GetCoreLogger()->flush()
+#define ST_CORE_DEBUG(...) ::Satoshi::Log::GetCoreLogger()->debug(__VA_ARGS__); ::Satoshi::Log::GetCoreLogger()->flush()
+#define ST_CORE_INFO(...) ::Satoshi::Log::GetCoreLogger()->info(__VA_ARGS__); ::Satoshi::Log::GetCoreLogger()->flush()
+#define ST_CORE_WARN(...) ::Satoshi::Log::GetCoreLogger()->warn(__VA_ARGS__); ::Satoshi::Log::GetCoreLogger()->flush()
+#define ST_CORE_ERR(...) ::Satoshi::Log::GetCoreLogger()->error(__VA_ARGS__); ::Satoshi::Log::GetCoreLogger()->flush()
+#define ST_CORE_CRITICAL(...) ::Satoshi::Log::GetCoreLogger()->critical(__VA_ARGS__); ::Satoshi::Log::GetCoreLogger()->flush()
 
-#define ST_TRACE(...) ::Satoshi::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define ST_DEBUG(...) ::Satoshi::Log::GetClientLogger()->debug(__VA_ARGS__)
-#define ST_INFO(...) ::Satoshi::Log::GetClientLogger()->info(__VA_ARGS__)
-#define ST_WARN(...) ::Satoshi::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define ST_ERR(...) ::Satoshi::Log::GetClientLogger()->error(__VA_ARGS__)
-#define ST_CRITICAL(...) ::Satoshi::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define ST_TRACE(...) ::Satoshi::Log::GetClientLogger()->trace(__VA_ARGS__); ::Satoshi::Log::GetClientLogger()->flush()
+#define ST_DEBUG(...) ::Satoshi::Log::GetClientLogger()->debug(__VA_ARGS__); ::Satoshi::Log::GetClientLogger()->flush()
+#define ST_INFO(...) ::Satoshi::Log::GetClientLogger()->info(__VA_ARGS__); ::Satoshi::Log::GetClientLogger()->flush()
+#define ST_WARN(...) ::Satoshi::Log::GetClientLogger()->warn(__VA_ARGS__); ::Satoshi::Log::GetClientLogger()->flush()
+#define ST_ERR(...) ::Satoshi::Log::GetClientLogger()->error(__VA_ARGS__); ::Satoshi::Log::GetClientLogger()->flush()
+#define ST_CRITICAL(...) ::Satoshi::Log::GetClientLogger()->critical(__VA_ARGS__); ::Satoshi::Log::GetClientLogger()->flush()
 
 #endif // !LOG_H
